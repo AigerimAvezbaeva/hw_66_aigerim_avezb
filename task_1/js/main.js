07b19a77fffa12cf12f99f343315918f810ecd11
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-04-04 08:04:25 +0600

    Добавлено первое задание

---
+++ task_1/js/main.js
@@ -0,0 +1,8 @@
+console.log(document.querySelector('#string-3').textContent)
+console.log(document.querySelector('#string-5').textContent)
+console.log(document.querySelector('#string-2').textContent)
+console.log(document.querySelector('#string-6').textContent)
+console.log(document.querySelector('#string-4').textContent)
+console.log(document.querySelector('#string-1').textContent)
+
+

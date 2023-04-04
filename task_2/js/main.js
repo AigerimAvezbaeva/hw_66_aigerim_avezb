b3aa0740615275db76cb80b82b40c229123bc6e3
Aigerim Avezbaeva <av21m1999@gmail.com>
2023-04-04 08:14:34 +0600

    Добавлено второе задание

---
+++ task_2/js/main.js
@@ -0,0 +1,8 @@
+const elements = document.querySelectorAll('.element')
+
+for (let i = 0; i < 3; i++) {
+  elements[i].style.color = 'red';
+}
+for (let i = 3; i < 6; i++) {
+  elements[i].style.color = 'green';
+}
